6aa374c207669951a8c320d0f8b5e6fa5b118fb1
Bimalendu Sarkar <bimalendu.s@technovert.com>
2020-03-02 18:30:51 +0530

    Added a space

--- CarPool/ShowAvailablePools.cs
+++ CarPool/ShowAvailablePools.cs
@@ -55,7 +55,7 @@ namespace CarPool
             Console.WriteLine("\n Source:{0}\n Destination:{1}\n Type of Vehicle:{2}\n Seats Available:{3}\n Name: {4}\n Phone:{5}\n Gender:{6}\n", poolProviderDetails.Source, poolProviderDetails.Destination, vehicleDetailsList.Find(x => x.Id.Equals(poolProviderDetails.Vehicle)).TypeOfVehicle == 2 ? "Two Wheeler" : "Four Wheeler", vehicleDetailsList.Find(x => x.Id.Equals(poolProviderDetails.Vehicle)).SeatsAvailable - poolProviderDetails.Riders.Count, poolProviderDetails.Name, poolProviderDetails.Phone, poolProviderDetails.Gender);
             if (poolProviderDetails.Riders.Count > 0)
             {
-                Console.WriteLine("Riders:");
+                Console.WriteLine(" Riders:");
                 foreach (int rider in poolProviderDetails.Riders)
                 {
                     UserDetailsStructure user = userDetails.getUserDetails(rider);
